c284c445f11fa7b25e944e09f4451939953c4a35
Ihor Pavliuk <akira.pavl@gmail.com>
2025-10-01 17:36:13 +0300

    fixed images missing in build

--- src/js/2-gallery/script.ts
+++ src/js/2-gallery/script.ts
@@ -6,11 +6,13 @@ const thumbBar = queryElement<HTMLElement>(".thumb-bar");
 const btn = queryElement<HTMLButtonElement>("button");
 const overlay = queryElement<HTMLElement>(".overlay");
 
-const images = Array(5)
-  .fill("")
-  .map((_, i) => `images/pic${i + 1}.jpg`);
+const images = import.meta.glob("./images/*.jpg", { eager: true }) as Record<
+  string,
+  { default: string }
+>;
+const imageUrls = Object.values(images).map((url) => url.default);
 const alts = ["Eye", "White pattern", "Flowers", "Egyptian picture", "Butterfly"];
-for (const [i, img] of images.entries()) {
+for (const [i, img] of imageUrls.entries()) {
   const newImage = document.createElement("img");
   newImage.setAttribute("src", img);
   newImage.setAttribute("alt", alts[i] || "");
